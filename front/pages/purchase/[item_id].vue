0e667eefadc454e6659abf233010f76c58569644
fukuda983667 <sfu983667@gmail.com>
2024-11-20 08:10:23 +0900

    商品購入画面

---
+++ front/pages/purchase/[item_id].vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>item_id : {{ itemId }}</h1>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'default',
+});
+
+const route = useRoute()
+// 動的パラメータを取得
+const itemId = route.params.item_id
+</script>
