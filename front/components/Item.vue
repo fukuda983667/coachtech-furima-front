73a2a4d54c9e1eeed507afd5142c909d9e414274
fukuda983667 <sfu983667@gmail.com>
2024-11-30 20:49:09 +0900

    Soldラベル表示

--- front/components/Item.vue
+++ front/components/Item.vue
@@ -1,12 +1,17 @@
 <template>
     <nuxt-link :to="{ name: 'item-item_id', params: { item_id: item.id } }" class="item">
         <!-- 商品画像 -->
-        <img :src="item.image_path" alt="商品画像" class="item__image" v-if="item.image_path" />
-
+        <div class="item__image__wrapper">
+            <img :src="item.image_path" alt="商品画像" class="item__image" v-if="item.image_path" />
+            <div v-if="item.isSold" class="item__sold__label">
+                <span class="item__sold__text">Sold</span>
+            </div>
+        </div>
         <!-- 商品名 -->
         <p class="item__name">{{ item.name }}</p>
     </nuxt-link>
 </template>
+
 
 <script setup>
 import { defineProps } from 'vue'
@@ -20,17 +25,46 @@ const props = defineProps({
 
 </script>
 
+
 <style lang="scss" scoped>
 .item {
     text-decoration: none;
     color: black;
     cursor: pointer;
 
-    .item__image {
+    .item__image__wrapper {
+        position: relative;
         width: 100%;
-        aspect-ratio: 1 / 1; /* 正方形のアスペクト比を設定 */
-        object-fit: cover;   /* 短い方を基準に画像を中央に表示 */
+        aspect-ratio: 1 / 1;
         border-radius: 4px;
+        overflow: hidden;
+
+        .item__image {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .item__sold__label {
+            position: absolute;
+            top: -23%; /* 上から5%移動 */
+            left: -23%; /* 左から5%移動 */
+            width: 45%;
+            height: 45%;
+            background-color: red;
+            display: flex;
+            align-items: flex-end; /* 下側に沿わせる */
+            justify-content: center;
+            transform: rotate(-45deg); /* 45度回転 */
+            transform-origin: center; /* 回転の基準を図形の中央に設定 */
+
+            .item__sold__text {
+                margin-bottom: 4%; /* 下側に余白を追加 */
+                color: white;
+                font-size: clamp(8px, 1.6vw, 24px);
+                font-weight: bold;
+            }
+        }
     }
 
     .item__name {
@@ -38,5 +72,4 @@ const props = defineProps({
         margin: 0;
     }
 }
-
 </style>
